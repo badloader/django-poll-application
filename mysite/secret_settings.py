515272ff1a847aa9fd1afe937be2835bbc4995ff
alireza <alirezakam10@gmail.com>
2021-08-23 18:24:39 +0430

    add secret settings

--- mysite/secret_settings.py
+++
@@ -1,3 +0,0 @@
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^v#m-9qlc5rt1-a*49=e$%v%wy^=5xada6c-@&2na&*#5!opl^'
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from .secret_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,6 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-^v#m-9qlc5rt1-a*49=e$%v%wy^=5xada6c-@&2na&*#5!opl^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
